33038726893a2f0381d06eab439e177d281bbcc8
Taily Vieira Gonçalves <126624205+TailyVieira@users.noreply.github.com>
2025-11-26 20:44:49 -0300

    Adicionando arquivos Dijkstra

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        try{
         Pacote[] pacotes = {
                 new Pacote(5, 10, 7),
                 new Pacote(8, 14, 6),
@@ -19,5 +22,32 @@ public class Main {
         for (Pacote p : r.selecionados) {
             System.out.println("- Peso: " + p.peso + " | Valor: " + p.valor + " | Prazo: " + p.prazo);
         }
+
+        Grafo grafo = new Grafo(6);
+            grafo.adicionarAresta(0, 1, 4);
+            grafo.adicionarAresta(0, 2, 2);
+            grafo.adicionarAresta(2, 3, 1);
+            grafo.adicionarAresta(1, 3, 5);
+            grafo.adicionarAresta(3, 4, 3);
+            grafo.adicionarAresta(4, 5, 2);
+
+            System.out.println("\n=== (ROTA FINAL) ===");
+
+            List<Integer> ordem = new ArrayList<>();
+            ordem.add(0);
+            ordem.add(2);
+            ordem.add(3);
+            ordem.add(4);
+            ordem.add(5);
+
+            Roteirizacao.RotaFinal rota = Roteirizacao.calcularRota(grafo, ordem);
+
+            System.out.println("\nRota final(Ordem): " + rota.rota);
+            System.out.println("Custo total da rota: " + rota.custoTotal);
+
+        } catch (Exception e) {
+            e.printStackTrace();
     }
 }
+}
+
